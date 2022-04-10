5a4ad116de345dcd984a5c66527dec8f395a5e9e
MarieCHessler <marie@hessler.se>
2022-04-10 11:02:55 +0000

    Add Alert Santa test that fails

--- test_student.py
+++ test_student.py
@@ -9,6 +9,13 @@ class TestStudent(unittest.TestCase):
         student = Student("John", "Doe")
         self.assertEqual(student.full_name, "John Doe")
 
+    def test_alert_santa(self):
+        """ Method to alert Santa if student has been naughty"""
+        student = Student("John", "Doe")
+        student.alert_santa()
+
+        self.assertTrue(student.naughty_list)
+
 
 if __name__ == "__main__":
     unittest.main()
