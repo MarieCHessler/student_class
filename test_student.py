7d0b5cd53ecabc263816de01d19dcf022fb0211c
MarieCHessler <marie@hessler.se>
2022-04-10 12:39:39 +0000

    Add setUp and tearDown methods to clean up code

--- test_student.py
+++ test_student.py
@@ -4,22 +4,29 @@ from student import Student
 
 class TestStudent(unittest.TestCase):
     """Class for testing, that inherits from unittest"""
+    def setUp(self):
+        print("setUp")
+        self.student = Student("John", "Doe")
+
+    def tearDown(self):
+        print("tearDown")
+
     def test_full_name(self):
         """ Test full name, with reference to self"""
-        student = Student("John", "Doe")
-        self.assertEqual(student.full_name, "John Doe")
-        
+        print("test_full_name")
+        self.assertEqual(self.student.full_name, "John Doe")
+
     def test_email(self):
         """ Test email """
-        student = Student("John", "Doe")
-        student.alert_santa()
-        self.assertEqual(student.email, "john.doe@email.com")
+        print("test_email")
+        self.student.alert_santa()
+        self.assertEqual(self.student.email, "john.doe@email.com")
 
     def test_alert_santa(self):
         """ Test Santa alert if student has been naughty"""
-        student = Student("John", "Doe")
-        student.alert_santa()
-        self.assertTrue(student.naughty_list)
+        print("test_alert_santa")
+        self.student.alert_santa()
+        self.assertTrue(self.student.naughty_list)
 
 
 if __name__ == "__main__":
